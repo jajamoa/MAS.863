41776f76bb1ad4f1baacc05009ea023f3f5aee72
LeeJAJA <jiajie@mit.edu>
2023-12-21 03:37:30 -0500

    added asset

--- compress.sh
+++ compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the path to the directory containing the folders
-root_path="./content/post/assets/week8"
+root_path="./content/post/assets/final"
 
 # Set the size threshold in bytes
 size_threshold_video=10000000
